3b6388925149897de1f6aeddcb870ab6ae206306
Maxim Torchinsky <roadtopatience@gmail.com>
2022-04-04 18:48:50 +0300

    feat: add hideTabContent in tabs.js

--- js/modules/tabs.js
+++ js/modules/tabs.js
@@ -3,6 +3,19 @@ function tabs(tabWrapper, windowTabs, tabsHeaders, tabHeader, tabActive) {
 		tabContent = tabContainer.querySelectorAll(windowTabs),
 		tabsParent = document.querySelector(tabsHeaders),
 		tabs = tabsParent.querySelectorAll(tabHeader);
+
+	let idTabs;
+
+	function hideTabContent() {
+		tabContent.forEach(item => {
+			item.classList.add('hide');
+			item.classList.remove('show', 'anim');
+		});
+
+		tabs.forEach(item => {
+			item.classList.remove(tabActive);
+		});
+	}
 }
 
 export default tabs;
