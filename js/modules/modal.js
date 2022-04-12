b5404da544fcf41bd58eac5f5474c47a0b27a7d5
Maxim Torchinsky <roadtopatience@gmail.com>
2022-04-12 21:53:03 +0300

    feat: add functional in modal.js

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -29,6 +29,25 @@ function modal(modalSelector, btnsOpenModal, timerOpenModal) {
 			window.removeEventListener('scroll', openModalByScroll);
 		}
 	}
+
+	btnsOpenModalWindow.forEach(btn => {
+		btn.addEventListener('click', () => openModalWindow(modalSelector, timerOpenModal));
+		btnCloseModalWindow.addEventListener('click', () => closeModalWindow(modalSelector));
+	});
+
+	modalWindow.addEventListener('click', (e) => {
+		if (e.target === modalWindow) {
+			closeModalWindow(modalSelector);
+		}
+	});
+
+	document.addEventListener('keydown', (e) => {
+		if (e.code === 'Escape' && modalWindow.classList.contains('show')) {
+			closeModalWindow(modalSelector);
+		}
+	});
+
+	window.addEventListener('scroll', openModalByScroll);
 }
 
 export default modal;
